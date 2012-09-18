f00274147bb321137dded9dffafe1d9cd3646b7c
webonise <webonise@webonise-desktop.(none)>
2012-09-18 11:10:15 +0530

    output files and recipedesc.rb has been added (3rd commit)

--- RecipeDesc.rb
+++ RecipeDesc.rb
@@ -7,24 +7,28 @@ ActiveRecord::Base.establish_connection(
 :host => "localhost",
 :username => "root",
 :password => "webonise6186", 
-:database=> "scrape",
+:database=> "recipesdb",
 )  
-  
-
+#creating connection to mysql  
+class Recipe < ActiveRecord::Base  
+end			#mapping class to database
 class RecipeDesc < ActiveRecord::Base
-end
+end			#mapping class to database
 url = "http://www.simplyrecipes.com/recipe-index.php"
-doc = Nokogiri::HTML(open(url))
-puts doc.at_css("title").text
-links = doc.search('a').map { |a| [a['href'],a.content]}
-for i in 0...links.length
-puts links[i][0].to_s + "================="
-url=links[i][0]
-doc = Nokogiri::HTML(open(url))
-str1 =doc.at_css(".entry-photo img").attribute('src').to_s
-str2 = doc.at_css(".center p").content
-str3 =doc.at_css(".ingredient").content
-str4 =doc.at_css("#recipe-method").content
-Recipedesc.create(:imagesrc => str1, :descr => str2, :ingredients => str3, :method => str4)
-puts str1
+doc = Nokogiri::HTML(open(url))						#opening main url 
+links = doc.search('a').map { |a| [a['href'],a.content]}		#copying all links from page to links array
+for i in 6...10
+Recipe.create(:link => links[i][0], :name => links[i][1])
+#for putting data into recipes table containing recipe name and link
+end
+for
+url1=links[i][0]
+doc1 = Nokogiri::HTML(open(url1))				#opening inner links for retrieving data
+str1 =doc1.at_css(".entry-photo img")	
+imgsrc=str1.attribute('src').to_s				#copying image src to local variable
+descrp = doc1.at_css(".center p").content		#copying the description
+ingrnt =doc1.at_css("#ingredient").content		#copying the ingredients
+metd =doc1.at_css("#recipe-method").content		#copying method into local variable
+RecipeDesc.create(:imagesrc => imgsrc, :descr => descrp, :ingredients => ingrnt, :method => metd)
+#for putting data into recipe_descs table 
 end
