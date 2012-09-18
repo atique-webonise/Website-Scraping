38cb9bf6e011f15e8940b036b0e8c70dcf474707
webonise <webonise@webonise-desktop.(none)>
2012-09-18 11:35:56 +0530

    RecipeDesc.rb updated only for five records

--- RecipeDesc.rb
+++ RecipeDesc.rb
@@ -17,17 +17,24 @@ end			#mapping class to database
 url = "http://www.simplyrecipes.com/recipe-index.php"
 doc = Nokogiri::HTML(open(url))						#opening main url 
 links = doc.search('a').map { |a| [a['href'],a.content]}		#copying all links from page to links array
-for i in 6...10
+for i in 10...15
 Recipe.create(:link => links[i][0], :name => links[i][1])
 #for putting data into recipes table containing recipe name and link
+puts links[i][0]
 end
-for
-url1=links[i][0]
+for i in 10...15
+url1 = links[i][0]
+puts url1
 doc1 = Nokogiri::HTML(open(url1))				#opening inner links for retrieving data
 str1 =doc1.at_css(".entry-photo img")	
+puts str1
+if str1.to_s == ''								#for checking str1 contains img tag or not
+imgsrc=nil
+else
 imgsrc=str1.attribute('src').to_s				#copying image src to local variable
+end
 descrp = doc1.at_css(".center p").content		#copying the description
-ingrnt =doc1.at_css("#ingredient").content		#copying the ingredients
+ingrnt =doc1.at_css("#recipe-ingredients").content		#copying the ingredients
 metd =doc1.at_css("#recipe-method").content		#copying method into local variable
 RecipeDesc.create(:imagesrc => imgsrc, :descr => descrp, :ingredients => ingrnt, :method => metd)
 #for putting data into recipe_descs table 
